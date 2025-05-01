c374d220e4baab8e996f367fe8e346b7f7747d6c
Özge Boztaş <ozgeboztas26@gmail.com>
2025-05-01 17:50:55 +0300

    tailwind

--- app/models/models.py
+++ app/models/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from app import db, login_manager
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash # type: ignore
 
 @login_manager.user_loader
 def load_user(user_id):
